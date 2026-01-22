a005b097daef75e5a10335bfcb98e4755030255c
NemethBalazsHBSZ <nemeth.balazs@hbsz.edu.hu>
2026-01-22 09:36:52 +0100

    commentek

--- HasznaltHangszerek-main/PROJECT/scripts/validation.js
+++ HasznaltHangszerek-main/PROJECT/scripts/validation.js
@@ -7,6 +7,8 @@ const passwordInput = document.getElementById("password-input");
 const repasswordInput = document.getElementById("repassword-input");
 const errorMessage = document.getElementById("error-message");
 
+var url = "http://localhost:5260/api/User";
+
 form.addEventListener("submit", (e) => {
   let errors = [];
 
@@ -18,17 +20,58 @@ form.addEventListener("submit", (e) => {
       birthdateInput.value,
       emailInput.value,
       passwordInput.value,
-      repasswordInput.value
+      repasswordInput.value,
     );
-    
+    if (errors.length > 0) {
+      e.preventDefault();
+      errorMessage.innerText = `Hiba: ${errors.join(" ")}`;
+    } else {
+      //SIGNUP HERE!!!
+    }
   } else {
     //Login
     errors = getLoginFormErrors(emailInput.value, passwordInput.value);
-  }
 
-  if (errors.length > 0) {
-    e.preventDefault();
-    errorMessage.innerText = `Hiba: ${errors.join(" ")}`;
+    if (errors.length > 0) {
+      e.preventDefault();
+      errorMessage.innerText = `Hiba: ${errors.join(" ")}`;
+    } else {
+      e.preventDefault();
+      // EZ A SIGNUP LESZ:
+      //
+      // fetch(url, {
+      //   method: "POST",
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      //   body: JSON.stringify({
+      //     Email: emailInput.value,
+      //     Password: passwordInput.value,
+      //   }),
+      //   mode: "no-cors",
+      // })
+      //   .then((response) => {
+      //     if (!response.ok) {
+      //       if (response.status < 500 && response.status >= 400) {
+      //         errorMessage.innerText = "Helytelen email cím vagy jelszó!";
+      //       } else {
+      //         errorMessage.innerText =
+      //           "Szerver hiba. Kérlek próbáld meg később!";
+      //         throw new Error(`HTTP hiba! Státuszkód: ${response.status}`);
+      //       }
+      //     } else {
+      //       alert("Sikeres bejelentkezés");
+      //       //és a továbbiak
+      //     }
+      //     return response.json();
+      //   })
+      //   .then((data) => {
+      //     //a továbbiak
+      //   })
+      //   .catch((error) => {
+      //     console.error("Hiba történt:", error);
+      //   });
+    }
   }
 });
 
@@ -38,70 +81,72 @@ function getSignupFormErrors(
   birthdate,
   email,
   password,
-  repassword
+  repassword,
 ) {
-    let errors = [];
-    if (lastname === "" || lastname == null) {
-      errors.push("A vezetéknév szükséges!");
-      lastnameInput.parentElement.classList.add("incorrect");
-    }
-    if (firstname === "" || firstname == null) {
-      errors.push("A keresztnév szükséges!");
-      firstnameInput.parentElement.classList.add("incorrect");
-    }
-    if (birthdate === "" || birthdate == null) {
-      errors.push("A születési dátum szükséges!");
-      birthdateInput.parentElement.classList.add("incorrect");
-    }
-    if (email === "" || email == null) {
-      errors.push("Az email szükséges!");
-      emailInput.parentElement.classList.add("incorrect");
-    } else if(!email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)){
-      errors.push("Nem megfelelő email cím!");
-      emailInput.parentElement.classList.add("incorrect");
-    }
-    if (password === "" || password == null) {
-      errors.push("A jelszó szükséges!");
-      passwordInput.parentElement.classList.add("incorrect");
-    } else if (password.length < 8) {
-      errors.push("A jelszónak legalább 8 karakter hosszúnak kell lennie!");
-      passwordInput.parentElement.classList.add("incorrect");
-    } else if(!password.match(/\d+/)){
-      errors.push("A jelszónak legalább 1 számot kell tartalmaznia!");
-      passwordInput.parentElement.classList.add("incorrect");
-    } else if(!password.match(/[@$!%*#?&-]+/)){
-      errors.push("A jelszónak legalább 1 speciális karaktert kell tartalmaznia!");
-      passwordInput.parentElement.classList.add("incorrect");
-    } else if(!password.match(/[A-Z]+/)){
-      errors.push("A jelszónak legalább 1 nagybetűt kell tartalmaznia!");
-      passwordInput.parentElement.classList.add("incorrect");
-    }
-    if (repassword === "" || repassword == null) {
-      errors.push("A jelszó megerősítése szükséges!");
-      repasswordInput.parentElement.classList.add("incorrect");
-    } else if (repassword != password) {
-      errors.push("A megadott jelszó nem egyezik!");
-      passwordInput.parentElement.classList.add("incorrect");
-      repasswordInput.parentElement.classList.add("incorrect");
-    }
-    const allInputs = [
-      lastnameInput,
-      firstnameInput,
-      birthdateInput,
-      emailInput,
-      passwordInput,
-      repasswordInput,
-    ];
+  let errors = [];
+  if (lastname === "" || lastname == null) {
+    errors.push("A vezetéknév szükséges!");
+    lastnameInput.parentElement.classList.add("incorrect");
+  }
+  if (firstname === "" || firstname == null) {
+    errors.push("A keresztnév szükséges!");
+    firstnameInput.parentElement.classList.add("incorrect");
+  }
+  if (birthdate === "" || birthdate == null) {
+    errors.push("A születési dátum szükséges!");
+    birthdateInput.parentElement.classList.add("incorrect");
+  }
+  if (email === "" || email == null) {
+    errors.push("Az email szükséges!");
+    emailInput.parentElement.classList.add("incorrect");
+  } else if (!email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+    errors.push("Nem megfelelő email cím!");
+    emailInput.parentElement.classList.add("incorrect");
+  }
+  if (password === "" || password == null) {
+    errors.push("A jelszó szükséges!");
+    passwordInput.parentElement.classList.add("incorrect");
+  } else if (password.length < 8) {
+    errors.push("A jelszónak legalább 8 karakter hosszúnak kell lennie!");
+    passwordInput.parentElement.classList.add("incorrect");
+  } else if (!password.match(/\d+/)) {
+    errors.push("A jelszónak legalább 1 számot kell tartalmaznia!");
+    passwordInput.parentElement.classList.add("incorrect");
+  } else if (!password.match(/[@$!%*#?&-]+/)) {
+    errors.push(
+      "A jelszónak legalább 1 speciális karaktert kell tartalmaznia!",
+    );
+    passwordInput.parentElement.classList.add("incorrect");
+  } else if (!password.match(/[A-Z]+/)) {
+    errors.push("A jelszónak legalább 1 nagybetűt kell tartalmaznia!");
+    passwordInput.parentElement.classList.add("incorrect");
+  }
+  if (repassword === "" || repassword == null) {
+    errors.push("A jelszó megerősítése szükséges!");
+    repasswordInput.parentElement.classList.add("incorrect");
+  } else if (repassword != password) {
+    errors.push("A megadott jelszó nem egyezik!");
+    passwordInput.parentElement.classList.add("incorrect");
+    repasswordInput.parentElement.classList.add("incorrect");
+  }
+  const allInputs = [
+    lastnameInput,
+    firstnameInput,
+    birthdateInput,
+    emailInput,
+    passwordInput,
+    repasswordInput,
+  ];
 
-    allInputs.forEach((input) => {
-      input.addEventListener("input", () => {
-        if (input.parentElement.classList.contains("incorrect")) {
-          input.parentElement.classList.remove("incorrect");
-          errorMessage.innerText = "";
-        }
-      });
+  allInputs.forEach((input) => {
+    input.addEventListener("input", () => {
+      if (input.parentElement.classList.contains("incorrect")) {
+        input.parentElement.classList.remove("incorrect");
+        errorMessage.innerText = "";
+      }
     });
-    return errors;
+  });
+  return errors;
 }
 
 function getLoginFormErrors(email, password) {
