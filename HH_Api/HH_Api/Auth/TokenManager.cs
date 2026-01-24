fa9e55e0ecb0f348d6f88c94fee7e370428e1e9a
Simon <79712743+SDominik5@users.noreply.github.com>
2026-01-24 21:29:34 +0100

    auth start

--- HH_Api/HH_Api/Auth/TokenManager.cs
+++ HH_Api/HH_Api/Auth/TokenManager.cs
@@ -1,6 +1,37 @@
-﻿namespace HH_Api.Auth
+﻿using HH_Api.Model;
+using System;
+using System.Configuration;
+using System.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+    
+
+namespace HH_Api.Auth
 {
     public class TokenManager
     {
+        private string _secretKey = string.Empty;
+        private string _issuer = string.Empty;
+        private string _audience = string.Empty;
+        
+        private Dictionary<string, List<string>> _rolesPermissions = [];
+
+        public List<string> Permissions
+        {
+            get
+            {
+                var permissions = new List<string>();
+                foreach (var values in _rolesPermissions.Values)
+                {
+                    permissions.AddRange(values);
+                }
+                return permissions;
+            }
+        }
+
+        public TokenManager(Configuration configuration)
+        {
+            
+        }
     }
 }
