75365c94b8528dbef23b45b907242331b1313a88
Simon <79712743+SDominik5@users.noreply.github.com>
2026-01-25 16:33:05 +0100

    auth not finished yet

--- HH_Api/HH_Api/Auth/TokenManager.cs
+++ HH_Api/HH_Api/Auth/TokenManager.cs
@@ -1,10 +1,7 @@
 ﻿using HH_Api.Model;
-using System;
-using System.Configuration;
 using System.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
-    
 
 namespace HH_Api.Auth
 {
@@ -29,9 +26,61 @@ namespace HH_Api.Auth
             }
         }
 
-        public TokenManager(Configuration configuration)
+        public TokenManager(ConfigurationManager configuration)
         {
+            _secretKey = configuration["Auth:JWT:Key"]!;
+            _issuer = configuration["Auth:JWT:Issuer"]!;
+            _audience = configuration["Auth:JWT:Audience"]!;
             
+            foreach (var role in configuration.GetSection("Auth:Roles")?.GetChildren() ?? [])
+            {
+                foreach (var permission in role.GetChildren())
+                {
+                    if (!string.IsNullOrEmpty(permission.Value))
+                    {
+                        if (!_rolesPermissions.TryGetValue(role.Key, out var permissionList))
+                        {
+                            permissionList = [];
+                            _rolesPermissions.Add(role.Key, permissionList);
+                        }
+                        permissionList.Add(permission.Value);
+                    }
+                }
+            }  
+            
+        }
+        public string GenerateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                // Az email címet mint a felhasználó "nevét" tároljuk el.
+                new (ClaimTypes.Name, user.Email)
+            };
+            foreach (var permission in _rolesPermissions[user.Role!])
+            {
+                // A felhasználó szerepköréhez tartozó jogosultságokat mint "engedélyeket"
+                // tároljuk el.
+                claims.Add(new Claim("permission", permission));
+            }
+
+            // A token aláírása a titkos kulcsból létrehozott "credentials" adatokkal történik.
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secretKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            // A token tartalmazza
+            var token = new JwtSecurityToken(
+                // a kibocsátót,
+                issuer: _issuer,
+                // a célközönséget,
+                audience: _audience,
+                // a fent összegyűjtött állításokat,
+                claims: claims,
+                // a lejárat időpontját,
+                expires: DateTime.Now.AddHours(1),
+                // és az aláíráshoz szükséges hitelesítési adatokat.
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
--- HH_Api/HH_Api/obj/Debug/net8.0/HH_Api.AssemblyInfo.cs
+++ HH_Api/HH_Api/obj/Debug/net8.0/HH_Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HH_Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa9e55e0ecb0f348d6f88c94fee7e370428e1e9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9187d3fde4396289638c64ab8498f49ae962890e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HH_Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HH_Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
