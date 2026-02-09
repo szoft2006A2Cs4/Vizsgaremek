5a150eb3bb63d7bce3d43f6ad365a9976af2ee4a
Simon <79712743+SDominik5@users.noreply.github.com>
2026-02-09 17:42:57 +0100

    nav, aboutUs

--- asd/hasznalthangszerek/src/assets/Components/aboutUs.jsx
+++ asd/hasznalthangszerek/src/assets/Components/aboutUs.jsx
@@ -1,3 +1,14 @@
+import Nav from "./Nav";
+import Footer from "./Footer";
+
 export default function AboutUs() {
-  return {};
+  return (
+    <div id="aboutUs">
+      <Nav></Nav>
+      <div id="aboutUs-main">
+        <h2>Rólunk</h2>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 }
