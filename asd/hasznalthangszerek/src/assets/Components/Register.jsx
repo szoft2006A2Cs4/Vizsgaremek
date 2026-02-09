03e2dc41b30fcbb8936165d5b25912287c2e4971
Simon <79712743+SDominik5@users.noreply.github.com>
2026-02-09 11:00:59 +0100

    register update

--- asd/hasznalthangszerek/src/assets/Components/Register.jsx
+++ asd/hasznalthangszerek/src/assets/Components/Register.jsx
@@ -252,11 +252,12 @@ const Register = () => {
           setErrMsg("Hálózati hiba vagy a szerver nem elérhető.");
         }
         errRef.current.focus();
+
+        for (let x of allInputs) {
+          x.setAttribute("disabled", false);
+        }
+        submitButton.setAttribute("disabled", false);
       }
-      for (let x of allInputs) {
-        x.setAttribute("disabled", false);
-      }
-      submitButton.setAttribute("disabled", false);
     }
   };
 
@@ -306,7 +307,7 @@ const Register = () => {
   }
 
   return (
-    <>
+    <div className="regist-container">
       {success ? (
         <div className="wrapper">
           <h1 id="successful">A regisztráció sikeres!</h1>
@@ -465,21 +466,16 @@ const Register = () => {
                 />
               </div>
               <div>
-                <label
-                  htmlFor="password-input"
-                  id="password-label"
-                  onClick={showPassword}
-                  dangerouslySetInnerHTML={{__html: document.getElementById("password-input").type === "password" ? `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8e3eD"><path d="M240-80q-33 0-56.5-23.5T160-160v-400q0-33 23.5-56.5T240-640h40v-80q0-83 58.5-141.5T480-920q83 0 141.5 58.5T680-720v80h40q33 0 56.5 23.5T800-560v400q0 33-23.5 56.5T720-80H240Zm240-200q33 0 56.5-23.5T560-360q0-33-23.5-56.5T480-440q-33 0-56.5 23.5T400-360q0 33 23.5 56.5T480-280ZM360-640h240v-80q0-50-35-85t-85-35q-50 0-85 35t-35 85v80Z"/></svg>` : `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8e3eD"><path d="M536.5-303.5Q560-327 560-360t-23.5-56.5Q513-440 480-440t-56.5 23.5Q400-393 400-360t23.5 56.5Q447-280 480-280t56.5-23.5ZM240-80q-33 0-56.5-23.5T160-160v-400q0-33 23.5-56.5T240-640h280v-80q0-83 58.5-141.5T720-920q83 0 141.5 58.5T920-720h-80q0-50-35-85t-85-35q-50 0-85 35t-35 85v80h120q33 0 56.5 23.5T800-560v400q0 33-23.5 56.5T720-80H240Z"/></svg>`}}
-                >
-                  // <svg
-                  //   xmlns="http://www.w3.org/2000/svg"
-                  //   height="24px"
-                  //   viewBox="0 -960 960 960"
-                  //   width="24px"
-                  //   fill="#e8e3eD"
-                  // >
-                  //   <path d="M240-80q-33 0-56.5-23.5T160-160v-400q0-33 23.5-56.5T240-640h40v-80q0-83 58.5-141.5T480-920q83 0 141.5 58.5T680-720v80h40q33 0 56.5 23.5T800-560v400q0 33-23.5 56.5T720-80H240Zm240-200q33 0 56.5-23.5T560-360q0-33-23.5-56.5T480-440q-33 0-56.5 23.5T400-360q0 33 23.5 56.5T480-280ZM360-640h240v-80q0-50-35-85t-85-35q-50 0-85 35t-35 85v80Z" />
-                  // </svg>
+                <label id="password-label" onClick={showPassword}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="24px"
+                    viewBox="0 -960 960 960"
+                    width="24px"
+                    fill="#e8e3eD"
+                  >
+                    <path d="M240-80q-33 0-56.5-23.5T160-160v-400q0-33 23.5-56.5T240-640h40v-80q0-83 58.5-141.5T480-920q83 0 141.5 58.5T680-720v80h40q33 0 56.5 23.5T800-560v400q0 33-23.5 56.5T720-80H240Zm240-200q33 0 56.5-23.5T560-360q0-33-23.5-56.5T480-440q-33 0-56.5 23.5T400-360q0 33 23.5 56.5T480-280ZM360-640h240v-80q0-50-35-85t-85-35q-50 0-85 35t-35 85v80Z" />
+                  </svg>
                 </label>
                 <input
                   type="password"
@@ -497,11 +493,7 @@ const Register = () => {
                 />
               </div>
               <div>
-                <label
-                  htmlFor="repassword-input"
-                  id="repassword-label"
-                  onClick={showRePassword}
-                >
+                <label id="repassword-label" onClick={showRePassword}>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     height="24px"
@@ -537,7 +529,7 @@ const Register = () => {
           </p>
         </div>
       )}
-    </>
+    </div>
   );
 };
 
