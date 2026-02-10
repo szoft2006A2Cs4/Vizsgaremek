a8fa0bd525869625700cac8eed0e6098a5a67feb
SimonDominikHBSZ <simon.dominik@hbsz.edu.hu>
2026-02-10 12:27:42 +0100

    Create Home.jsx

---
+++ asd/hasznalthangszerek/src/assets/Components/Home.jsx
@@ -0,0 +1,16 @@
+import "../../index.css";
+import Nav from "./Nav";
+import Introduction from "./Introduction";
+import PseMain from "./pseMain";
+
+function Home() {
+  return (
+    <>
+      <Nav />
+      <Introduction />
+      <PseMain />
+    </>
+  );
+}
+
+export default Home;
