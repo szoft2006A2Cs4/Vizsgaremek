c9885a266c05116d7e070dc08c7abb2887363f61
Czibók Bence <czibok.bence@hbsz.edu.hu>
2026-02-10 13:02:24 +0100

    asda

--- asd/hasznalthangszerek/src/assets/Components/Footer.jsx
+++ asd/hasznalthangszerek/src/assets/Components/Footer.jsx
@@ -3,13 +3,45 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 export default function Footer() {
   return ( <footer>
-    <section>
-      <hr />
-      <section>
-        <a href="">Social</a>
+    <section className="footer" >
+      <hr className="footer-seperator" />
+      <section className="footer-social-media" >
+        <a href="https://www.instagram.com/hasznalthangszerek/">Social</a>
       </section>
-
-    </section>
+      <section className="footer-info" >
+        <section className="footer-info-left" >
+          <section className="footer-info__name">
+              Használt Hangszerek
+          </section>
+          <section className="footer-info__returns">
+            Ide még kell valami
+            <br />
+            Ide is
+          </section>
+        </section>
+        <section className="footer-info-center">
+          <section className="footer-info__email">
+              hhproject67@gmail.com
+          </section>
+          <section className="footer-info__terms">
+            Ide még kell valami
+            <br />
+            Ide is
+          </section>
+        </section>
+        </section>
+        <section className="footer-info-right">
+          <section className="footer-info__number">
+              06301234567
+          </section>
+          <section className="footer-info__contact">
+            <Link to="/login">Bejelentkezés</Link>
+            <br />
+            <Link to="/register">Regisztrálás</Link>
+          </section>
+        </section>
+        <hr className="footer-seperator" />
+        </section>
     </footer>
   )
 }
