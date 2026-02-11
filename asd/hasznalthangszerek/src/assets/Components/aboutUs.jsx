6f9d1ce6fbd9ab5a46ca8d93fa8ce49b64c33c0d
Simon <79712743+SDominik5@users.noreply.github.com>
2026-02-11 09:55:40 +0100

    aboutUs page finished

--- asd/hasznalthangszerek/src/assets/Components/aboutUs.jsx
+++ asd/hasznalthangszerek/src/assets/Components/aboutUs.jsx
@@ -9,28 +9,31 @@ export default function AboutUs() {
       <Nav></Nav>
       <div id="aboutUs-main">
         <div id="aboutUs-Introtext">
-          <h2>Rólunk</h2>
+          <h1>Rólunk</h1>
 
-          <h4>
+          <h3>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
             incidunt, nobis commodi tempora pariatur placeat necessitatibus,
             debitis, eum molestiae cupiditate quos repudiandae repellat esse
             voluptatum quibusdam amet iusto quasi? Quia!
-          </h4>
+          </h3>
         </div>
 
         <div id="aboutUs-Creatortext">
           <AboutUsCard
             name="Czibók Bence"
             img="https://res.cloudinary.com/dknhbvrq9/image/upload/v1770717706/czibi_v0d1hd.jpg"
+            subTitle="Cigány"
           />
           <AboutUsCard
             name="Németh Balázs"
             img="https://res.cloudinary.com/dknhbvrq9/image/upload/v1770717705/balazs_wvopjo.jpg"
+            subTitle="BodzaSzörpötIvó"
           />
           <AboutUsCard
             name="Simon Dominik"
             img="https://res.cloudinary.com/dknhbvrq9/image/upload/v1770717707/B2517D2F-B470-4764-A85D-24C49023EFE9_1_105_c_cr7q8c.jpg"
+            subTitle="ÉlőSoundboard"
           />
         </div>
       </div>
