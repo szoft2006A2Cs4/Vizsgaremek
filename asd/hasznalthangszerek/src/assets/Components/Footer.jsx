289fcc37cd4ee3f5e0247cb9dd7a9cd6f72e1bdb
Czibók Bence <czibok.bence@hbsz.edu.hu>
2026-02-12 09:53:10 +0100

    elvileg kész footer

--- asd/hasznalthangszerek/src/assets/Components/Footer.jsx
+++ asd/hasznalthangszerek/src/assets/Components/Footer.jsx
@@ -1,47 +1,39 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-
+import { Link } from "react-router-dom";
+import { FaInstagram } from "react-icons/fa";
 
 export default function Footer() {
-  return ( <footer>
-    <section className="footer" >
-      <hr className="footer-seperator" />
-      <section className="footer-social-media" >
-        <a href="https://www.instagram.com/hasznalthangszerek/">Social</a>
-      </section>
-      <section className="footer-info" >
-        <section className="footer-info-left" >
-          <section className="footer-info__email">
-              hhproject67@gmail.com
-          </section>
-          <section className="footer-info__returns">
-            Rólunk
-            <br />
-            Ide is
-          </section>
-        </section>
-        <section className="footer-info-center">
-          <section className="footer-info__name">
-              Használt Hangszerek
-          </section>
-          <section className="footer-info__terms">
-            Általános Szerződési Feltételek
-            <br />
-            Visszaküldési szabályzat
-          </section>
-        </section>
-        <section className="footer-info-right">
-          <section className="footer-info__number">
-              06301234567
-          </section>
-          <section className="footer-info__contact">
-            <Link to="/login">Bejelentkezés</Link>
-            <br />
-            <Link to="/register">Regisztrálás</Link>
-          </section>
-        </section>
-        </section>
-        <hr className="footer-seperator" />
-        </section>
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+
+        <div className="footer-column">
+          <h3>Kapcsolat</h3>
+          <p>hhproject67@gmail.com</p>
+          <p>06301234567</p>
+          <a href="https://www.instagram.com/hasznalthangszerek/" target="_blank" className="social-link">
+          
+          <FaInstagram className="social-icon" /> Instagram</a>
+        </div>
+
+        <div className="footer-column center">
+          <h2 className="footer-logo">Használt Hangszerek</h2>
+          <p>Kiváló minőségű hangszerek kedvező áron</p>
+          <Link to="/login">Bejelentkezés</Link>
+          <Link to="/register">Regisztráció</Link>
+        </div>
+
+        <div className="footer-column">
+          <h3>Információ</h3>
+          
+          <p>Általános Szerződési Feltételek</p>
+          <p>Visszaküldési szabályzat</p>
+        </div>
+
+      </div>
+
+      <div className="footer-bottom">
+        © {new Date().getFullYear()} Használt Hangszerek – Minden jog fenntartva
+      </div>
     </footer>
-  )
+  );
 }
