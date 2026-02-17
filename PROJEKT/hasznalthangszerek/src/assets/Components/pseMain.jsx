4ec78890bcbb431a03f38e7f91d5b3f00a74e0aa
NemethBalazsHBSZ <nemeth.balazs@hbsz.edu.hu>
2026-02-17 11:59:31 +0100

    article

--- PROJEKT/hasznalthangszerek/src/assets/Components/pseMain.jsx
+++ PROJEKT/hasznalthangszerek/src/assets/Components/pseMain.jsx
@@ -1,9 +1,12 @@
 import Footer from "./Footer";
+import ArticleAds from "./ArticleAds";
 
 export default function PseMain() {
   return (
     <main>
-      <article>article</article>
+      <article>
+        <ArticleAds />
+      </article>
       <section>section</section>
       <Footer />
     </main>
