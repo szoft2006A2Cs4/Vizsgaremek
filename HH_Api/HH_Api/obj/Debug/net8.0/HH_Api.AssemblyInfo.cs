b136366802cccebcd54e82eafabeecee7a1c92e6
Simon <79712743+SDominik5@users.noreply.github.com>
2026-02-19 11:07:21 +0100

    imageId update

--- HH_Api/HH_Api/obj/Debug/net8.0/HH_Api.AssemblyInfo.cs
+++ HH_Api/HH_Api/obj/Debug/net8.0/HH_Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HH_Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c46539b5f0ea94d34346ea5089aebfdd053c9f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9385b9f4c1bf457b907018d7cc38414582f85fd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("HH_Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HH_Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
