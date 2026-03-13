fe27b7f73d22b372cb1d88b3f6b6f88d30e9e81d
SimonDominikHBSZ <simon.dominik@hbsz.edu.hu>
2026-03-13 08:07:28 +0100

    Create adCardTemp.jsx

---
+++ MainPROJECT/hasznalthangszerek/src/assets/scripts/adCardTemp.jsx
@@ -0,0 +1,23 @@
+class AdCardTemp {
+  Id;
+  IName;
+  Cost;
+  Desc;
+  Condition;
+  IsPremium;
+  SCat;
+  UId;
+  Imgs;
+
+  constructor(id, iName, cost, desc, condition, isPremium, sCat, uId, imgs) {
+    this.Id = id;
+    this.IName = iName;
+    this.Cost = cost;
+    this.Desc = desc;
+    this.Condition = condition;
+    this.IsPremium = isPremium;
+    this.SCat = sCat;
+    this.UId = uId;
+    this.Imgs = imgs;
+  }
+}
