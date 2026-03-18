aa4f7e8cf48940edca76467e6cd988abe972f768
Simon <79712743+SDominik5@users.noreply.github.com>
2026-03-18 10:51:27 +0100

    forgot password
    kinezet, vissza gomb csak az email kuldes resz

--- MainPROJECT/hasznalthangszerek/src/assets/Components/forgottenPassword.jsx
+++ MainPROJECT/hasznalthangszerek/src/assets/Components/forgottenPassword.jsx
@@ -1,31 +1,51 @@
-import { Card, Field } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 export default function ForgottenPassword() {
+  const navigate = useNavigate();
+
+  function HandleResetEmail() {}
+
   return (
     <div id="forgot-page">
-      <Card.Root id="forgot-card">
-        <Card.Header>
-          <h2>Elfelejtett jelszó</h2>
-        </Card.Header>
-        <Card.Body>
-          <h4>
-            Adja meg azt az email-címét amivel regisztrált az oldalon és
-            elfelejtette a jelszavát!
-          </h4>
-          <div id="forgot-styled-wrapper">
-            <label htmlFor="email-input" className="forgot-label">
-              <span>@</span>
-            </label>
-            <input
-              type="text"
-              name="email"
-              id="forgot-styled-input"
-              placeholder="Email"
-              autoComplete="off"
-            />
-          </div>
-        </Card.Body>
-      </Card.Root>
+      <div id="forgot-card">
+        <h2>Elfelejtett jelszó</h2>
+
+        <h4>
+          Adja meg azt az email-címét amivel regisztrált az oldalon és
+          elfelejtette a jelszavát!
+        </h4>
+        <div id="forgot-styled-wrapper">
+          <label htmlFor="email-input" id="forgot-label">
+            <span>@</span>
+          </label>
+          <input
+            type="text"
+            name="email"
+            id="forgot-styled-input"
+            placeholder="Email"
+            autoComplete="off"
+          />
+        </div>
+
+        <div id="forgot-btn-cont">
+          <button
+            className="uni-button"
+            onClick={() => {
+              navigate("/", { replace: true });
+            }}
+          >
+            Mégsem
+          </button>
+          <button
+            className="uni-button"
+            onClick={() => {
+              HandleResetEmail();
+            }}
+          >
+            Küldés
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
